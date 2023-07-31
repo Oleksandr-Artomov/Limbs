7fc39cc32328e9a8e9ccca327926cbdc9c97b60e
KeithLim <85144995+KeithL1m@users.noreply.github.com>
2023-07-31 00:20:02 -0700

    Minor fixes and added throwing limb input

--- Assets/Scripts/Player/Limb.cs
+++ Assets/Scripts/Player/Limb.cs
@@ -48,7 +48,7 @@ public class Limb : MonoBehaviour
 
     }
 
-    private void OnCollisionEnter2D(Collision collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
